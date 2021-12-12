b845e22159c45cb7293da4968a68d7263fe1f4b2
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-12 12:29:30 -0600

    Completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+        @color = ""
+        @size = 0
+        @material = ""
+        @condition = "new"
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
